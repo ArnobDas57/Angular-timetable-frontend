13226fb941293fa964ccb03530876ffbde827cf1
Arnob Das <adas46@uwo.ca>
2020-11-16 18:17:59 -0500

    Created and fixed routes to each of the created components

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { CreateSchedComponent } from './create-sched/create-sched.component';
+import { DisplayComponent } from './display/display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'display', component: DisplayComponent },
+  { path: 'create-sched', component: CreateSchedComponent }
+];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
